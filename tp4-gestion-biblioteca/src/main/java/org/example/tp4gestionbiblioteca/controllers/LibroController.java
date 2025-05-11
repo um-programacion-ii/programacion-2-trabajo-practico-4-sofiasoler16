6df2c4091156537276a801173cb6fc6865bae41a
s.soler <s.soler@alumno.um.edu.ar>
2025-05-11 14:39:19 -0300

    Implementado Controlador Libro

---
+++ tp4-gestion-biblioteca/src/main/java/org/example/tp4gestionbiblioteca/controllers/LibroController.java
@@ -0,0 +1,42 @@
+package org.example.tp4gestionbiblioteca.controllers;
+
+import org.example.tp4gestionbiblioteca.models.Libro;
+import org.example.tp4gestionbiblioteca.services.LibroService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/libros")
+public class LibroController {
+    private final LibroService libroService;
+
+    public LibroController(LibroService libroService) {
+        this.libroService = libroService;
+    }
+
+    @GetMapping
+    public List<Libro> obtenerTodos() {
+        return libroService.obtenerTodos();
+    }
+
+    @GetMapping("/{isbn}")
+    public Libro obtenerPorIsbn(@PathVariable String isbn) {
+        return libroService.buscarPorIsbn(isbn);
+    }
+
+    @PostMapping
+    public Libro crear(@RequestBody Libro libro) {
+        return libroService.guardar(libro);
+    }
+
+    @PutMapping("/{id}")
+    public Libro actualizar(@PathVariable Long id, @RequestBody Libro libro) {
+        return libroService.actualizar(id, libro);
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable Long id) {
+        libroService.eliminar(id);
+    }
+}
